13fc4fb3c510f744f34748677ccd61aaa0e32c41
shreyash nawghare <shreyashnawghare@gmail.com>
2021-10-26 21:23:13 +0530

    pricing card

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ReactPricingCard from './Components/ReactPricingCard';
+function App() {
+  return (
+    <div  className="pricing py-5">
+      {/* <PricingCard /> */}
+      <ReactPricingCard />
+
+    </div>
+  );
+}
+
+export default App;
